c47591b6587fb1671fc65de6b8feaf64083876c8
Fred <fredank@tutamail.com>
2018-05-27 00:44:41 -0300

    Reimplemented the property and dependency system

--- douglas-inject/src/main/java/com/github/foskel/douglas/plugin/dependency/supply/DependencySupplyingException.java
+++
@@ -1,7 +0,0 @@
-package com.github.foskel.douglas.plugin.dependency.supply;
-
-public class DependencySupplyingException extends RuntimeException {
-    public DependencySupplyingException(String message) {
-        super(message);
-    }
-}
--- douglas-inject/src/main/java/com/github/foskel/douglas/plugin/dependency/supply/DependencySupplyingStrategy.java
+++
@@ -1,5 +0,0 @@
-package com.github.foskel.douglas.plugin.dependency.supply;
-
-public interface DependencySupplyingStrategy {
-    void scanAndSupply(Object source) throws DependencySupplyingException;
-}
